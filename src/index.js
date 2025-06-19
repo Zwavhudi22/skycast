e36360d4e36457b168e95701b4df69c4c13b068c
Zwavhudi22 <nengalavhaniz@gmail.com>
2025-06-19 03:23:03 +0200

    Adding functionality to the search engine

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+}
+
+function showTemperature(city) {
+  let apiKey = "t39a377fcb5bac04bc0ecf418e394foa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function displayCity(event) {
   event.preventDefault();
-  let city = document.querySelector("#search-input");
   let cityName = document.querySelector("#current-city");
+  let city = document.querySelector("#search-input");
   cityName.innerHTML = city.value;
 }
 
